0553b3c2f00641417e365eacb3adc08d17b6830e
shu chen <chensche@cisco.com>
2015-02-04 12:46:09 +0800

    update server.py to verify database operation

--- measuring-system/Measuringsystem/server.py
+++ measuring-system/Measuringsystem/server.py
@@ -1,20 +1,21 @@
 import os, sys
-os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "Measuringsystem.settings"
 
 from django.contrib.auth.models import User
-from datahandle.models import Meter
-from datahandle.models import Data
+from MeterManagment.models import Meter
+from MeterManagment.models import Data
 import django
 django.setup()
 
 from socket import *
 import datetime
+import struct
 
-HOST = '10.140.92.51'
+HOST = '10.140.92.41'
 PORT = 20000
-BUFSIZ = 1024
+BUFSIZE = 1024
 ADDR = (HOST, PORT)
-
+new_id = 0
 SerSock = socket(AF_INET, SOCK_DGRAM)
 SerSock.bind(ADDR)
 
@@ -24,43 +25,30 @@ while True:
     print 'received message from:', addr
 
     if data:
+        if len(data) < 12:
+            continue
         head = data[0:12]
-        version = head[0]
-        meter_type = hear[1]
-        eui = head[2:9]
-        data_type = head[10]
-        lenth = head[11]
-        payload = data[12:12+lenth-1]
+        version, meter_type, eui, data_type, length = struct.unpack("!BB8sBB", head) 
 
-        if version != 1:
-            continue
- 
+    if version != 1:
+        continue
+
+    if data_type == 6:
+        payload = data[12:(12+length)]
+        qb, vb1, vb2 = struct.unpack("!IIH", payload)
+        qb_s = str(qb)
+        vb_s = str(vb1)+'.'+str(vb2)
+        print qb_s+'  '+vb_s
+        new_id = new_id + 1
+        new_data = Data(data_id=new_id, data_vb=vb_s, data_qb=qb_s)
+        new_data.data_date = datetime.datetime.now()
+        new_data.save()
+
         try:
-            meter=Meter.objects.get(meter_eui=eui)      
+            data_list = Data.objects.all()
+            print ("data num:%d" %(len(data_list)))
+            print data_list[0].data_vb+' '+data_list[0].data_qb
         except (KeyError, Meter.DoesNotExist):
-            print ("do not exist\n" %(eui))
-
-        if not meter:
-            continue
-
-        try:
-            data_t=Data.objects.get(meter_id=meter.meter_id) 
-        except (KeyError, Data.DoesNotExist):
-            print ("do not exist\n")
-
-        if data_t:
-            data_l = data.object.order_by('data_id')[0]
-            new_id = data_l.data_id + 1
-        else:
-            new_id = 1
-
-        if data_type = 6:
-            qb = socket.ntohl(payload[0:3])
-            qb_s = str(qb)
-            vb1 = socket.ntohl(payload[4:7])
-            vb2 = socket.ntohs(payload[8:9])
-            vb_s = str(vb1)+'.'+str(vb2)
-            new_data = Data(data_id=new_id, data_vb=vb_s, data_qb=qb_s)
-            new_data.save()
-
+            print ("%s do not exist\n" %(eui))
+ 
 SerSock.close()
